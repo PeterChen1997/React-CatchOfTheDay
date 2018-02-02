40bc4139dbceef174f31fabee7bda8613b1a4166
PeterChen <747853229@qq.com>
2018-02-02 17:51:41 +0800

    finished v18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBEFekqeVI0O_LBm_y2mnQJt7MsJsSzqVc",
+    authDomain: "catch-of-the-day-c8f14.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-c8f14.firebaseio.com"
+})
+
+export default base
