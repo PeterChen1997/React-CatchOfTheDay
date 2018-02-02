659ba75491db767dc3926c120f3bb18322f57149
PeterChen <747853229@qq.com>
2018-02-02 15:18:03 +0800

    finished v16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
 
         this.addFish = this.addFish.bind(this)
         this.loadSamples = this.loadSamples.bind(this)
+        this.addToOrder = this.addToOrder.bind(this)
     }
 
     loadSamples() {
@@ -32,6 +33,12 @@ class App extends React.Component {
         this.setState({ fishes })
     }
 
+    addToOrder(index) {
+        const order = this.state.order
+        order[index] = order[index] + 1 || 1
+        this.setState({ order })
+    }
+
     render () {
         return (
             <div className="catch-of-the-day">
@@ -40,7 +47,7 @@ class App extends React.Component {
                     <ul className="list-of-fishes">
                         {Object
                             .keys(this.state.fishes)
-                            .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                            .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
                         }
                     </ul>
                 </div>
